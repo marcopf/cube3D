4be52e9a0b4000f44f7b2bcc518f7b4ccc40ccde
Alessandro Fraccalvieri <afraccal@e3r11p3.42roma.it>
2023-05-09 14:27:26 +0200

    cube3D.h
    norminette

--- main.c
+++ main.c
@@ -6,28 +6,28 @@
 /*   By: afraccal <afraccal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 14:18:28 by marco             #+#    #+#             */
-/*   Updated: 2023/05/09 14:05:22 by afraccal         ###   ########.fr       */
+/*   Updated: 2023/05/09 14:27:05 by afraccal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3D.h"
 
-int arg_check(int argc, char **argv)
+int	arg_check(int argc, char **argv)
 {
-    int	i;
+	int	i;
 
 	i = ft_strlen (argv[1]) - 4;
     if (argc < 2 || argc > 2)
-        return (printf ("Numero argomenti sbagliato\n"));
-    if (!ft_strncmp(argv[1][i], ".cub", 4))
+		return (printf ("Numero argomenti sbagliato\n"));
+	if (!ft_strncmp(argv[1][i], ".cub", 4))
 		return (printf ("Estensione file errata\n"));
-    return (0);
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
 	t_game	game;
-	
+
 	if (!arg_check(argc, argv))
 		return (1);
 	init_game(&game);
